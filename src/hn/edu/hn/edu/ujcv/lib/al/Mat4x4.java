9111a99f62ce7d85042fdcea028130a1b814abab
Guillertosso <guillertosso@gmail.com>
2018-11-12 08:31:55 -0600

    Creando Mat4x4

--- src/hn/edu/hn/edu/ujcv/lib/al/Mat4x4.java
+++ src/hn/edu/hn/edu/ujcv/lib/al/Mat4x4.java
@@ -1,4 +1,18 @@
 package hn.edu.hn.edu.ujcv.lib.al;
 
 public class Mat4x4 {
+
+    private VecR4 colX;
+    private VecR4 colY;
+    private VecR4 colZ;
+    private VecR4 colW;
+
+    public Mat4x4(){
+        colX = new VecR4(1,0,0,0);
+        colY = new VecR4(0,1,0,0);
+        colZ = new VecR4(0,0,1,0);
+        colW = new VecR4(0,0,0,1);
+    }
+
+
 }
