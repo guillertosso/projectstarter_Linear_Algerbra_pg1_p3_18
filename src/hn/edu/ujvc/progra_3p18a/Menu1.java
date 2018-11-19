b75fb4a3d43098a7ad579507c7091eaa1cd5e64a
Guillertosso <guillertosso@gmail.com>
2018-11-19 08:04:10 -0600

    menu prueba 2

--- src/hn/edu/ujvc/progra_3p18a/Menu1.java
+++ src/hn/edu/ujvc/progra_3p18a/Menu1.java
@@ -2,7 +2,7 @@ package hn.edu.ujvc.progra_3p18a;
 
 public class Menu1 {
     public static void menu(){
-        System.out.println("Menu 1");
-        
+        System.out.println("Menu prueba");
+
     }
 }
